5bc14789caf20a13270d4af7de2154f28f941f90
doohochang <doohochang.gmail.com>
2023-04-11 06:02:00 +0900

    feat: add header into summary

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -l
 
 $JAVA_HOME/bin/java -jar /app/openapi-diff.jar $1 $2 --markdown summary.md
+
+echo "# OpenAPI Diff Summary" >> $GITHUB_STEP_SUMMARY
 cat summary.md >> $GITHUB_STEP_SUMMARY
